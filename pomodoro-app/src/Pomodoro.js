e1eeb575e233e5ca7ec30c5396287d09e39cb7f5
Dev <devmiistry@gmail.com>
2023-12-24 15:58:34 -0500

    completed all functionality and tested

--- pomodoro-app/src/Pomodoro.js
+++ pomodoro-app/src/Pomodoro.js
@@ -1,47 +1,104 @@
 import React, { useState, useEffect } from "react";
 
 export default function Pomodoro() {
+    const STATUS = {
+        pause: 0,
+        start: 1,
+        default: 2
+    }
+    const TIMER = {
+        pomo: 0,
+        short: 1,
+        long: 2
+    }
     const [minutes, setMinutes] = useState(25);
     const [seconds, setSeconds] = useState(0);
     const [displayMessage, setDisplayMessage] = useState(false);
+    const [status, setStatus] = useState(STATUS.default);
+    const [timer, setTimer] = useState(TIMER.pomo)
+    const intervalRef = React.useRef();
 
-    //change logic - separate pomodoro & breaks
-    useEffect(() => {
-        //set interval every 1000ms
-        let interval = setInterval(() => {
-            clearInterval(interval); //always clear
-            if (seconds === 0) {
-                if (minutes !== 0) {
-                    setSeconds(59);
-                    setMinutes(minutes - 1);
-                } else {//timer finished
-                    //if displaymsg true(break done) then start next pomodoro session
-                    //else start break (5 minutes)
-                    let minutes = displayMessage ? 24 : 4;
-                    let seconds = 59;
-                    setMinutes(minutes);
-                    setSeconds(seconds);
-                    setDisplayMessage(!displayMessage);
+    function handler() {
+        if (seconds === 0) {
+            if (minutes === 0) {
+                if (timer === 0) {
+                    setDisplayMessage("Take a break! Next session starts in:");
+                    setTimer(TIMER.short);
+                    setMinutes(5);
+                    setSeconds(0);
+                } else {
+                    setDisplayMessage("Session completed!");
+                    setTimer(TIMER.pomo);
+                    setMinutes(25);
+                    setSeconds(0);
+                    setStatus(pause);
                 }
+                setDisplayMessage(!displayMessage);
             } else {
-                setSeconds(seconds - 1);
+                setMinutes(minutes - 1);
+                setSeconds(59);
             }
-         }, 1000)
-     }, [seconds]);
-   
+        } else {
+            setSeconds(seconds - 1);
+        }
+    };
+
+    useEffect(() => {
+        if (status === STATUS.start) {
+            intervalRef.current = setInterval(() => {
+                handler()
+            }, 1000);
+        } else if (status === STATUS.pause && intervalRef.current) {
+            clearInterval(intervalRef.current)
+        }
+        return () => {
+            clearInterval(intervalRef.current)
+        };
+    }, [minutes, seconds, status]);
+
+
     const timerMinutes = minutes < 10 ? `0${minutes}` : minutes;
     const timerSeconds = seconds < 10 ? `0${seconds}` : seconds;
 
+    const start = () => setStatus(STATUS.start);
+    const pause = () => setStatus(STATUS.pause);
+    const stop = () => {
+        setStatus(pause);
+        pomo();
+    }
+
+    const pomo = () => {
+        setTimer(TIMER.pomo);
+        setMinutes(25);
+        setSeconds(0);
+        setDisplayMessage("Time to focus! Take a break in:");
+    }
+    const short = () => {
+        setTimer(TIMER.short);
+        setMinutes(5);
+        setSeconds(0);
+        setDisplayMessage("A short break to recharge!");
+    }
+    const long = () => {
+        setTimer(TIMER.long);
+        setMinutes(15);
+        setSeconds(0);
+        setDisplayMessage("Take a longer break, you deserve it!");
+    }
+
     return <div className="pomodoro">
-        <button className="btn" id="timerBtns">POMODORO</button>
-        <button className="btn" id="timerBtns">SHORT BREAK</button>
-        <button className="btn" id="timerBtns">LONG BREAK</button>
+        <button className="btn" id="timerBtn1" onClick={() => pomo()}>POMODORO</button>
+        <button className="btn" id="timerBtn2" onClick={() => short()}>SHORT BREAK</button>
+        <button className="btn" id="timerBtn3" onClick={() => long()}>LONG BREAK</button>
         <div className="message">
-        {displayMessage && "Break time! Next session starts in:"}
+            {displayMessage}
         </div>
         <div className="timer">
             {timerMinutes}:{timerSeconds}
         </div>
-        <button className="btn" id="startBtn">START</button>
+        <button className="btn" id="startBtn" onClick={() => start()}>START</button>
+        <button className="btn" id="pauseBtn" onClick={() => pause()}>PAUSE</button>
+        <button className="btn" id="stopBtn" onClick={() => stop()}>RESET</button>
     </div>
+
 }
